07158d3d92b33422685e166f09d4ea42227a4293
Sandesh Rimal <sandesh.rimal@bison.howard.edu>
2020-03-22 23:40:22 -0400

    taking user input

--- recursion.s
+++ recursion.s
@@ -1,8 +1,15 @@
 .data
 	promptUser: .asciiz "Enter the value"
+	theString: .space 1000
+	#Error Messages
+	INVALID_INPUT: .asciiz  "Invalid input"
 
 .text
 	main:
+		#Taking user input as string
+		li $v0, 8
+		la $a0, theString
+		syscall
 
 
 
