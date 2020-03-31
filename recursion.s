6b6b556cad6f1a4303ef4d165c1e2bc67a817dd6
Sandesh Rimal <sandesh.rimal@bison.howard.edu>
2020-03-31 08:00:15 -0400

    loading the the byte in the function length

--- recursion.s
+++ recursion.s
@@ -27,3 +27,7 @@ helperFunction:
 		li $s7, 0				# Flag to help check space in the middle of string($s0 is 1 after first non space character is encountered, otherwise 0)
 		li $t4, 1				# Initializing power to be multiplied
 		li $s1, 30				# Base
+	li $t1, 0   # Counter, at the end $t1 will have number of characters(non space) in the string
+	length:
+		lb $t2, 0($t0)
+		beq $t2, 10, checkEmpty     # If \n is encountered move to converting part
