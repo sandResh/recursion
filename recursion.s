825b1859dddc592ea8482a6bc4dccff2021e2aec
Sandesh Rimal <sandesh.rimal@bison.howard.edu>
2020-03-31 23:40:39 -0400

    asking user input

--- recursion.s
+++ recursion.s
@@ -5,3 +5,8 @@ input_value_empty: .asciiz "Input is empty."
 input_value_invalid: .asciiz "Invalid base-27 number."
 input_too_long: .asciiz "Input is too long."
 
+.text
+main:
+li $v0, 8                                   #  syscall code to get user input
+la $a0, user_input                          #  loading byte space into address
+li $a1, 11000                               #  allocating byte space for input string
